32dd39e32cc2ee8a2ac5a0111b93736342559690
leejun <hubo.move@gmail.com>
2024-03-09 01:23:45 +0900

    [edit] main

--- main.py
+++ main.py
@@ -19,16 +19,10 @@ if __name__ == "__main__":
     print(responseReadBaudrate(rft.getResponse(ID_READ_BAUDRATE)))
     rft.sendCommand(COMMAND_START_FT_DATA_OUTPUT)
     time.sleep(0.1)
-    ok = False
-    while not ok:
-        rft.softTare()
-        # print(responseReadFTData(rft.getResponse(ID_START_FT_DATA_OUTPUT)))
-        print(rft.getTareFT())
-        time.sleep(1/100)
-        rft.ser.flush()
-        check = input("is ok? [y/n]")
-        if check == 'y' or check == 'Y' or check =='':
-            ok = True
+    rft.sendCommand(commandSetFilter(1,10))
+    time.sleep(0.1)
+    # rft.softTare()
+    rft.hardTare()
     while True:
         # print(responseReadFTData(rft.getResponse(ID_START_FT_DATA_OUTPUT)))
         print(f"{rft.getTareFT()[0]:>9.4f}, {rft.getTareFT()[1]:>9.4f}, {rft.getTareFT()[2]:>9.4f}, {rft.getTareFT()[3]:>9.4f}, {rft.getTareFT()[4]:>9.4f}, {rft.getTareFT()[5]:>9.4f}")
